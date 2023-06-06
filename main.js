33a0f6a79f19dbebd0f83593b724edf4d8011d4c
ansel brandt <ansel@anselbrandt.com>
2023-06-05 23:32:02 -0400

    Add d3 tree

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import { Tree } from "./tree";
+import * as data from "./flare.json";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#app").innerHTML = `<div id="graph"></div>`;
 
-setupCounter(document.querySelector('#counter'))
+const svg = document.getElementById("graph");
+const chart = Tree(data, {
+  label: (d) => d.name,
+  title: (d, n) =>
+    `${n
+      .ancestors()
+      .reverse()
+      .map((d) => d.data.name)
+      .join(".")}`, // hover text
+  link: (d, n) =>
+    `https://github.com/prefuse/Flare/${
+      n.children ? "tree" : "blob"
+    }/master/flare/src/${n
+      .ancestors()
+      .reverse()
+      .map((d) => d.data.name)
+      .join("/")}${n.children ? "" : ".as"}`,
+  width: 1152,
+});
+svg.append(chart);
